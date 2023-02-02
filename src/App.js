32c9feadcffdb362d6c3a48e739b03c69e2c08d4
braverokmc79 <braverokmc79@gmail.com>
2023-02-02 21:06:29 +0900

    18.할 일 목록 부분을 위한 컴포넌트 생성하기(컴포넌트 분리하기)

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
+import List from "./components/List";
 
 export default function App() {
 
   const [todoData, setTodoData] = useState([]);
   const [value, setValue] = useState("");
-
-
-
-  const btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right"
-  }
-
-  const getStyle = (completed) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none"
-
-    }
-  }
-
-
-
-  const handleClick = (id) => {
-    let newTodoData = todoData.filter(data => data.id !== id);
-    setTodoData(newTodoData);
-  }
 
   const handleChnage = (e) => {
     setValue(e.target.value);
@@ -48,20 +22,6 @@ export default function App() {
     setTodoData(prev => [...prev, newTodoData])
     setValue("");
   }
-
-
-  const handleCompleteChange = (id) => {
-    let newTodoData = todoData.map(data => {
-      if (data.id === id) {
-        data.completed = !data.completed
-      }
-      return data;
-    });
-
-    setTodoData(newTodoData);
-  }
-
-
 
   return (
     <div className="container" >
@@ -90,16 +50,9 @@ export default function App() {
         </form>
 
 
+        <List todoData={todoData} setTodoData={setTodoData} />
 
-        {todoData.map(data => {
-          return (
-            <div style={getStyle(data.completed)} key={data.id}>
-              <input type="checkbox" defaultChecked={data.completed} onChange={() => handleCompleteChange(data.id)} />
-              {data.title}
-              <button style={btnStyle} onClick={() => handleClick(data.id)}>x</button>
-            </div>
-          )
-        })}
+
 
       </div>
     </div >
