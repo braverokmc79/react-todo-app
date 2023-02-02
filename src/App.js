0f145a1f2040da0b150262684a79b3c89ac0a1bd
braverokmc79 <braverokmc79@gmail.com>
2023-02-02 22:36:20 +0900

    19.Form 부분을 위한 컴포넌트 생성하기

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
+import Form from "./components/Form";
 import List from "./components/List";
 
 export default function App() {
 
   const [todoData, setTodoData] = useState([]);
-  const [value, setValue] = useState("");
-
-  const handleChnage = (e) => {
-    setValue(e.target.value);
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    let newTodoData = {
-      id: Date.now(),
-      title: value,
-      completed: false
-    }
-
-    setTodoData(prev => [...prev, newTodoData])
-    setValue("");
-  }
 
   return (
     <div className="container" >
@@ -30,29 +14,9 @@ export default function App() {
           <h1>할일 목록</h1>
         </div>
 
-        <form style={{ display: 'flex' }} onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="value"
-            style={{ flex: "10", padding: "5px" }}
-            placeholder="해야 할일을 입력 하세요."
-            value={value}
-            onChange={handleChnage}
-          />
-
-          <input
-            type="submit"
-            name="입력"
-            className="btn"
-            style={{ flex: '1' }}
-
-          />
-        </form>
-
+        <Form setTodoData={setTodoData} />
 
         <List todoData={todoData} setTodoData={setTodoData} />
-
-
 
       </div>
     </div >
---
+++ src/components/Form.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+const Form = ({ setTodoData }) => {
+    const [value, setValue] = useState("");
+
+    const handleChnage = (e) => {
+        setValue(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let newTodoData = {
+            id: Date.now(),
+            title: value,
+            completed: false
+        }
+
+        setTodoData(prev => [...prev, newTodoData])
+        setValue("");
+    }
+
+    return (
+        <>
+            <form style={{ display: 'flex' }} onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="value"
+                    style={{ flex: "10", padding: "5px" }}
+                    placeholder="해야 할일을 입력 하세요."
+                    value={value}
+                    onChange={handleChnage}
+                />
+
+                <input
+                    type="submit"
+                    name="입력"
+                    className="btn"
+                    style={{ flex: '1' }}
+
+                />
+            </form>
+        </>
+    );
+};
+
+export default Form;
