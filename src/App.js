775a22aa142b41d1537dcfa132eb499bfbdcf5ee
braverokmc79 <braverokmc79@gmail.com>
2023-02-05 16:54:08 +0900

    29.할 일 리스트 모두 지우기 버튼 생성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import Lists from "./components/Lists";
@@ -7,7 +7,6 @@ export default function App() {
   console.log("App :");
 
   const [todoData, setTodoData] = useState([
-
     {
       id: 1,
       title: "공부하기",
@@ -20,11 +19,20 @@ export default function App() {
     },
 
   ]);
+
+  const handleRemoveClick = useCallback(() => {
+    setTodoData([]);
+  }, [setTodoData]);
+
+
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
       <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg ">
         <div className="flex justify-between mb-3">
-          <h1 className="justify-center text-2xl font-bold text-center">할일 목록</h1>
+          <h1 className="justify-center text-2xl font-bold text-center">
+            할일 목록
+          </h1>
+          <button onClick={handleRemoveClick} >Delete All</button>
         </div>
 
         <Form setTodoData={setTodoData} />
