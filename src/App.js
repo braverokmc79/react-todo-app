5592037ef949ae0067a13a275816f7e68a5ca352
braverokmc79 <braverokmc79@gmail.com>
2023-02-07 15:17:51 +0900

    쿠팡 파트너 적용

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React, { useState, useCallback } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import Lists from "./components/Lists";
+import styled from "styled-components";
+
 
 const initialTodoData = localStorage.getItem("todoData") ? JSON.parse(localStorage.getItem("todoData")) : [];
+const Iframe = styled.iframe`
+  width:1062px;
+  height:auto;
+  display: inline-block;
+`;
 
 export default function App() {
+
 
   const [todoData, setTodoData] = useState(initialTodoData);
   const handleRemoveClick = useCallback(() => {
@@ -17,23 +25,30 @@ export default function App() {
 
 
   return (
-    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
-      <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg ">
-        <div className="flex justify-between mb-3">
-          <h1 className="justify-center text-2xl font-bold text-center">
-            할일 목록
-          </h1>
-          <button
-            className='p-2 text-red-400 border-2 border-red-400 rounded
-             hover:text-white hover:bg-red-400 hover:cursor-pointer'
-            onClick={handleRemoveClick} >전체삭제</button>
-        </div>
-
-        <Form todoData={todoData} setTodoData={setTodoData} />
-
-        <Lists todoData={todoData} setTodoData={setTodoData} />
-
+    <>
+      <div className="flex justify-center bg-blue-100">
+        <Iframe src="https://ads-partners.coupang.com/widgets.html?id=637991&template=carousel&trackingCode=AF2176654&subId=&width=680&height=140"
+          frameborder="0" scrolling="no" referrerpolicy="unsafe-url"></Iframe>
       </div>
-    </div >
+      <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+
+        <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg ">
+          <div className="flex justify-between mb-3">
+
+            <h1 className="justify-center text-2xl font-bold text-center">
+              할일 목록
+            </h1>
+            <button
+              className='p-2 text-red-400 border-2 border-red-400 rounded
+             hover:text-white hover:bg-red-400 hover:cursor-pointer'
+              onClick={handleRemoveClick} >전체삭제</button>
+          </div>
+
+          <Form todoData={todoData} setTodoData={setTodoData} />
+
+          <Lists todoData={todoData} setTodoData={setTodoData} />
+        </div>
+      </div >
+    </>
   )
 }
